228a8e89bc74b751098b696f905f3ae1c6152608
liber <liberator8461071@gmail.com>
2020-11-07 14:07:17 -0600

    begin 1

--- app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/models/Course.java
+++ app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/models/Course.java
@@ -3,10 +3,38 @@ package edu.illinois.cs.cs125.fall2020.mp.models;
 /**
  */
 public class Course extends Summary {
+  private String description;
+
     /**
-    *
-    * @return description
-    */
+     * get description.
+     *
+     * @return description
+     */
   public String getDescription() {
-    return ""; }
+    return description; }
+
+    /**
+     * Empty Course.
+     */
+  @SuppressWarnings({"unused", "RedundantSuppression"})
+  public Course() { }
+
+    /**
+     * Course constructor.
+     * @param setYear super
+     * @param setSemester super
+     * @param setDepartment super
+     * @param setNumber super
+     * @param setTitle super
+     * @param setDescription sets description
+     */
+  public Course(final String setYear,
+                final String setSemester,
+                final String setDepartment,
+                final String setNumber,
+                final String setTitle,
+                final String setDescription) {
+    super(setYear, setSemester, setDepartment, setNumber, setTitle);
+    description = setDescription;
+  }
 }
