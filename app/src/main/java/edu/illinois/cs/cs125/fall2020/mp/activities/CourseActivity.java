ff1143d138869bd8acfa78558d85e75398a01908
liber <liberator8461071@gmail.com>
2020-11-07 13:32:43 -0600

    begin 1

--- app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/activities/CourseActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/activities/CourseActivity.java
@@ -1,7 +1,10 @@
 package edu.illinois.cs.cs125.fall2020.mp.activities;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 /**
  *
  */
-public class CourseActivity {
+public class CourseActivity extends AppCompatActivity {
+
 }
