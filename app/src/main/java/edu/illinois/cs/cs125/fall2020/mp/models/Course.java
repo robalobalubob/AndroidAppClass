9981308d2ee14986cac0c665e12d778e38855ffc
liber <liberator8461071@gmail.com>
2020-11-07 13:17:15 -0600

    begin 1

---
+++ app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/models/Course.java
@@ -0,0 +1,12 @@
+package edu.illinois.cs.cs125.fall2020.mp.models;
+
+/**
+ */
+public class Course extends Summary {
+    /**
+    *
+    * @return description
+    */
+  public String getDescription() {
+    return ""; }
+}
